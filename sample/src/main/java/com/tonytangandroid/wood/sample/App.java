9901da053560bb81ad6e6334eb468bc858076c7b
TonyTang <tonytang@uber.com>
2022-07-19 14:51:45 -0700

    Show verbose log.

--- sample/src/main/java/com/tonytangandroid/wood/sample/App.java
+++ sample/src/main/java/com/tonytangandroid/wood/sample/App.java
@@ -1,6 +1,7 @@
 package com.tonytangandroid.wood.sample;
 
 import android.app.Application;
+import android.util.Log;
 
 
 public class App extends Application {
@@ -15,6 +16,7 @@ public class App extends Application {
     private static void initWood(Application application) {
         timber.log.Timber.plant(new com.tonytangandroid.wood.WoodTree(application)
                 .retainDataFor(com.tonytangandroid.wood.WoodTree.Period.FOREVER)
+                .logLevel(Log.VERBOSE)
                 .autoScroll(false)
                 .maxLength(100000).showNotification(true));
 
